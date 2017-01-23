de62f09982b26c77333a51fba60625ee02697a61
WWlodek <tiggiss@gmail.com>
2017-01-23 18:39:51 +0100

    Update

--- src/application/Main.java
+++ src/application/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application {
 		try {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root, 1200, 700);
-			
+			primaryStage.setTitle("Quadratic Function Drawer");
 			// Add Elements to scene ================
 			HBox hbox1 = new HBox(8);
 			hbox1.setId("hbox1");
